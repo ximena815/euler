f766b507f646c4f56e790a2990c7e076b9ca35dd
ximena815 <68305685+ximena815@users.noreply.github.com>
2021-02-22 20:10:20 -0500

    Add files via upload

---
+++ euler/euler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace euler
+{
+    class Program
+    {
+        static double Factorial(double valor)
+        {
+            double total = 1;
+            for (int cont = 1; cont <= valor;cont++)
+            {
+                total *= cont;
+            }
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular a Euler");
+            double x = 3;
+            double euler = 0;
+            int n = 100;
+
+            
+            for (int i=0;i <= n; i++)
+            {
+               euler += (Math.Pow(x, i) / Factorial(i));
+            }
+            Console.WriteLine("Euler >:3..........." + euler);
+        }
+
+    }
+}
